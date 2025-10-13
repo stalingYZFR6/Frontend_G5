e87cab9d1f2558b675992733733efd1c704a97b4
stalingYZFR6 <stalinggosling@gmail.com>
2025-10-13 11:48:09 -0600

    personalizacion de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-import Login from "./Login";
+import { useState } from 'react'
+import './App.css'
+import imagen from './assets/zzzzzz.webp'; // tu imagen
 
-function App() {
-  return <Login />;
+const App = () => {
+  return (
+    <>
+      <h1>Sistema gestor de empleados</h1>
+      <img src={imagen} alt="Imagen de la página" />
+      <div className="buttons">
+        <button>Iniciar Sesión</button>
+        <button>Regístrate</button>
+      </div>
+    </>
+  )
+
 }
-
 export default App;
+
